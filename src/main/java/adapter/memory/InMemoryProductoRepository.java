b55e120eeb8eb6b6b8675158b2d23ceb2abfe116
Yan25102309 <rey.anamaria2006@gmail.com>
2026-09-19 12:56:18 -0600

    refactor(adapter-memory): corregir code smell detectado por SonarQube

--- src/main/java/adapter/memory/InMemoryProductoRepository.java
+++ src/main/java/adapter/memory/InMemoryProductoRepository.java
@@ -6,7 +6,6 @@ import domain.Producto;
 import java.util.ArrayList; 
 import java.util.List; 
 import java.util.Map; 
-import java.util.Objects; 
 import java.util.Optional; 
 import java.util.concurrent.ConcurrentHashMap;
 
